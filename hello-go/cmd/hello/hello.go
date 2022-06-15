aa05e56d7f1e7a0382cfbd9aabd13a582974ba06
Tony Dawson <tony.dawson@microlise.com>
2022-06-15 16:39:42 +0100

    Fixed Hostname lookup

--- hello-go/cmd/hello/hello.go
+++ hello-go/cmd/hello/hello.go
@@ -9,7 +9,13 @@ import (
 
 // HelloServer responds to requests with the given URL path.
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	hostname := os.Hostname()
+	hostname, err := os.Hostname()
+
+	if err != nil {
+		fmt.Println(err)
+		log.Fatalf("Could not get Hostname : error %v", err)
+		os.Exit(1)
+	}
 
 	fmt.Fprintf(w, "Hello, you requested: '%s' and were served by '%s'\n", r.URL.Path, hostname)
 	log.Printf("Received request for path: '%s' and were served by '%s'\n", r.URL.Path, hostname)
