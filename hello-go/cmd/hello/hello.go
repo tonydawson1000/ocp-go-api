81c90273a01e44a76a3042f95ceefcab5b62bf59
Tony Dawson <tony.dawson@microlise.com>
2022-06-15 16:23:40 +0100

    Refactor Hostname

--- hello-go/cmd/hello/hello.go
+++ hello-go/cmd/hello/hello.go
@@ -9,8 +9,10 @@ import (
 
 // HelloServer responds to requests with the given URL path.
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, you requested: '%s' and were served by '%s'\n", r.URL.Path, os.Hostname())
-	log.Printf("Received request for path: '%s' and were served by '%s'\n", r.URL.Path, os.Hostname())
+	hostname := os.Hostname()
+
+	fmt.Fprintf(w, "Hello, you requested: '%s' and were served by '%s'\n", r.URL.Path, hostname)
+	log.Printf("Received request for path: '%s' and were served by '%s'\n", r.URL.Path, hostname)
 }
 
 func main() {
